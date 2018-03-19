501b55a1ea96bf39a6f9d64f89abf90823d08d50
Ryan Greenlee <Ryan@kimberlys-mbp.wlan.rose-hulman.edu>
2018-03-19 13:36:14 -0400

    Typed up code

--- Lights Out/ViewController.swift
+++ Lights Out/ViewController.swift
@@ -10,16 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var gameBoardButtons: [UIButton]!
+    @IBOutlet weak var gameStateLabel: UILabel!
+    @IBOutlet weak var gameStateNavBar: UINavigationBar!
+    
+    var game = LinearLightsOutGame(numLights: 13)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        gameStateNavBar.titleTextAttributes = [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 20)]
+        updateView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func pressedNewGame(sender: Any) {
+        game = LinearLightsOutGame(numLights: 13)
+        updateView()
     }
-
-
+    
+    @IBAction func pressedGameBoardButton(_ sender: Any) {
+        let gameBoardButton = sender as! UIButton
+        game.pressedLightAtIndex(gameBoardButton.tag)
+        updateView()
+    }
+    
+    func updateView() {
+        var onImage: UIImage
+        var offImage: UIImage
+        onImage = #imageLiteral(resourceName: "light_on")
+        offImage = #imageLiteral(resourceName: "light_off")
+        
+        for i in 0..<13 {
+            let button = gameBoardButtons[i]
+            switch (game.lightStates[i]) {
+            case true:
+                button.setImage(onImage, for: UIControlState.normal)
+            case false:
+                button.setImage(offImage, for: UIControlState.normal)
+            }
+        }
+    }
 }
 
